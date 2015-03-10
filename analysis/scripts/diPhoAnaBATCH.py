a677a9e0834f66acc2779da346662bd230094ede
crovelli <crovelli@gmail.com>
2015-03-10 15:42:30 +0100

    first commit not to lose

---
+++ analysis/scripts/diPhoAnaBATCH.py
@@ -0,0 +1,39 @@
+import FWCore.ParameterSet.Config as cms
+import FWCore.Utilities.FileUtils as FileUtils
+
+process = cms.Process("diPhoAna")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.load("Configuration.StandardSequences.GeometryDB_cff")
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = 'POSTLS170_V5::All'
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32( 500 ) )
+
+process.source = cms.Source("PoolSource",
+                            fileNames=cms.untracked.vstring(
+        "/store/group/phys_higgs/cmshgg/musella/flashgg/ExoPhys14_v2/diphotonsPhys14V2/RSGravToGG_kMpl001_M_5000_Tune4C_13TeV_pythia8/ExoPhys14_v2-diphotonsPhys14V2-v0-Phys14DR-PU20bx25_PHYS14_25_V1-v1/150128_133931/0000/myOutputFile_1.root"
+        )
+                            )
+
+process.load("flashgg/MicroAODProducers/flashggPhotons_cfi")
+process.load("flashgg/MicroAODProducers/flashggDiPhotons_cfi")
+
+process.TFileService = cms.Service("TFileService",fileName = cms.string("OUTPUT"))
+
+process.diPhoAna = cms.EDAnalyzer('DiPhoAnalyzer',
+                                  VertexTag = cms.untracked.InputTag('offlineSlimmedPrimaryVertices'),
+                                  packedGenParticles = cms.untracked.InputTag('flashggGenPhotons'),
+                                  DiPhotonTag = cms.untracked.InputTag('flashggDiPhotons'),
+                                  PileupTag = cms.untracked.InputTag('addPileupInfo'),
+                                  dopureweight = PU,
+                                  sampleIndex  = SI,
+                                  puWFileName  = weights,
+                                  xsec         = XS,
+                                  kfac         = KF
+                                  )
+
+process.p = cms.Path(process.diPhoAna)
+
