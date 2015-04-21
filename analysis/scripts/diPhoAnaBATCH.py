95a314a1fea25031d4afb40583375915e6d40e5e
crovelli <crovelli@gmail.com>
2015-04-21 12:23:46 +0200

    moving to new Flashgg dataformats

--- analysis/scripts/diPhoAnaBATCH.py
+++ analysis/scripts/diPhoAnaBATCH.py
@@ -19,8 +19,8 @@ process.source = cms.Source("PoolSource",
                             fileNames = cms.untracked.vstring('/store/group/phys_higgs/cmshgg/musella/flashgg/ExoPhys14_v2/diphotonsPhys14V2/RSGravToGG_kMpl001_M_5000_Tune4C_13TeV_pythia8/ExoPhys14_v2-diphotonsPhys14V2-v0-Phys14DR-PU20bx25_PHYS14_25_V1-v1/150128_133931/0000/myOutputFile_1.root'
                                                               )
 
-process.load("flashgg/MicroAODProducers/flashggPhotons_cfi")
-process.load("flashgg/MicroAODProducers/flashggDiPhotons_cfi")
+process.load("flashgg/MicroAOD/flashggPhotons_cfi")
+process.load("flashgg/MicroAOD/flashggDiPhotons_cfi")
 
 process.TFileService = cms.Service("TFileService",fileName = cms.string("OUTPUT"))
 
