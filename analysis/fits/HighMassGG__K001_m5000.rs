21cec5bb7d3585841318fde1616a0fb4d3a2cc0f
crovelli <crovelli@gmail.com>
2015-05-11 15:15:53 +0200

    first commit

---
+++ analysis/fits/HighMassGG__K001_m5000.rs
@@ -0,0 +1,38 @@
+mgg[500,6000];
+mggGen[500,6000];
+nvtx[0,50];
+weight[0, 1000];
+eventClass[-10,10];
+
+ReducedMass_sig_mean_cat0[-0.01,-0.012,0.008];
+ReducedMass_sig_sigma_cat0[0.01, 0.005, 0.015];
+ReducedMass_sig_alphaCBneg_cat0[0.47, 0.2, 0.8]; 
+ReducedMass_sig_alphaCBpos_cat0[-2., -2.2, -1.5];
+ReducedMass_sig_frac_cat0[0.7, 0.5, 1.];
+ReducedMass_sig_nCBneg_cat0[10., 8., 12.];     
+ReducedMass_sig_nCBpos_cat0[1.5, 1., 2.];    
+
+ReducedMass_sig_mean_cat1[-0.005,-0.010,-0.002];
+ReducedMass_sig_sigma_cat1[0.007, 0.003, 0.01];
+#ReducedMass_sig_alphaCBpos_cat1[-2.5, -3., -0.1];
+ReducedMass_sig_alphaCBpos_cat1[-1.5, -3., -0.1];
+ReducedMass_sig_alphaCBneg_cat1[0.5, 0., 3.]; 
+ReducedMass_sig_nCBpos_cat1[6., 3., 10.];    
+ReducedMass_sig_nCBneg_cat1[4.5, 0., 5.];     
+ReducedMass_sig_frac_cat1[0.5, 0., 1.];
+
+ReducedMass_sig_mean_cat2[-0.004.,-0.01, 0.];
+ReducedMass_sig_sigma_cat2[0.01, 0.001, 0.1];
+ReducedMass_sig_alphaCBpos_cat2[-1., -3., -0.1];
+ReducedMass_sig_alphaCBneg_cat2[0.5, 0., 1.5]; 
+ReducedMass_sig_nCBpos_cat2[9., 6., 10.];    
+ReducedMass_sig_nCBneg_cat2[5., 4., 6.];     
+ReducedMass_sig_frac_cat2[0.5, 0., 1.];
+
+ReducedMass_sig_mean_cat3[-0.004,-0.01, 0.];
+ReducedMass_sig_sigma_cat3[0.01, 0.001, 0.1];
+ReducedMass_sig_alphaCBpos_cat3[-2., -4., -1.];
+ReducedMass_sig_alphaCBneg_cat3[2.6, 0., 4.];
+ReducedMass_sig_nCBpos_cat3[1.5, 0.5, 3.];
+ReducedMass_sig_nCBneg_cat3[1.5, 0.5, 3.];
+ReducedMass_sig_frac_cat3[0.5, 0., 1.];
