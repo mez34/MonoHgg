2d6e8ea9129b3a4db4cb2e1b0cc9f7eee0db9779
crovelli <crovelli@gmail.com>
2015-05-12 22:15:14 +0200

    new initialization with fixed value of N

--- analysis/fits/HighMassGG__K001_m750_unw.rs
+++ analysis/fits/HighMassGG__K001_m750_unw.rs
@@ -8,32 +8,32 @@ ReducedMass_sig_mean_cat0[-0.005,-0.01,0.001];
 ReducedMass_sig_sigma_cat0[0.005, 0.001, 0.01];
 ReducedMass_sig_alphaCBpos_cat0[-2.5, -3., -0.1];
 ReducedMass_sig_alphaCBneg_cat0[0.5, 0., 3.]; 
-ReducedMass_sig_nCBpos_cat0[6., 3., 11.];    
-ReducedMass_sig_nCBneg_cat0[4.5, 0., 5.];     
+ReducedMass_sig_nCBpos_cat0[5., 5., 5.];    
+ReducedMass_sig_nCBneg_cat0[5., 5., 5.];     
 ReducedMass_sig_frac_cat0[0.5, 0., 1.];
 
 ReducedMass_sig_mean_cat1[-0.003,-0.01,0.];
 ReducedMass_sig_sigma_cat1[0.008, 0.001, 0.01];
-ReducedMass_sig_alphaCBpos_cat1[-1.5, -3., -1.];
+ReducedMass_sig_alphaCBpos_cat1[-1.5, -3., 0.];
 ReducedMass_sig_alphaCBneg_cat1[0.5, 0., 1.]; 
-ReducedMass_sig_nCBpos_cat1[1., 0., 2.];    
-ReducedMass_sig_nCBneg_cat1[4.5, 3., 5.];     
+ReducedMass_sig_nCBpos_cat1[5., 5., 5.];    
+ReducedMass_sig_nCBneg_cat1[5., 5., 5.];     
 ReducedMass_sig_frac_cat1[0.5, 0., 1.];
 
 ReducedMass_sig_mean_cat2[-0.002.,-0.01, 0.];
 ReducedMass_sig_sigma_cat2[0.012, 0.001, 0.1];
 ReducedMass_sig_alphaCBpos_cat2[-1., -1.5, -0.5];
 ReducedMass_sig_alphaCBneg_cat2[0.5, 0., 1.]; 
-ReducedMass_sig_nCBpos_cat2[2.5, 1.5, 5.];    
-ReducedMass_sig_nCBneg_cat2[2.5, 2., 15.];     
+ReducedMass_sig_nCBpos_cat2[5., 5., 5.];    
+ReducedMass_sig_nCBneg_cat2[5., 5., 5.];     
 ReducedMass_sig_frac_cat2[0.5, 0., 1.];
 
 ReducedMass_sig_mean_cat3[-0.003,-0.01, 0.];
 ReducedMass_sig_sigma_cat3[0.01, 0.001, 0.1];
 ReducedMass_sig_alphaCBpos_cat3[-1., -2., -0.5];
 ReducedMass_sig_alphaCBneg_cat3[0.5, 0., 1.];
-ReducedMass_sig_nCBpos_cat3[2., 1., 4.];
-ReducedMass_sig_nCBneg_cat3[2., 0., 3.];
+ReducedMass_sig_nCBpos_cat3[5., 5., 5.];    
+ReducedMass_sig_nCBneg_cat3[5., 5., 5.];     
 ReducedMass_sig_frac_cat3[0.5, 0., 1.];
 
 
