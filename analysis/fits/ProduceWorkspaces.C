7910c9347f27c0123c512ce7a63769632a2d1670
crovelli <crovelli@gmail.com>
2015-05-19 16:22:34 +0200

    some cleaning done

--- analysis/fits/ProduceWorkspaces.C
+++ analysis/fits/ProduceWorkspaces.C
@@ -1,13 +1,14 @@
 #include "HighMassGGFitter.cc"
 //
 
-void ProduceWorkspaces(int mass, Double_t width, std::string model){
+void ProduceWorkspaces(int mass, std::string coupling, bool doBand, Double_t width){
   
   //gSystem->SetIncludePath("-I/afs/cern.ch/cms/slc5_amd64_gcc462/lcg/roofit/5.32.00-cms5/include");
   // gSystem->Load("libRooFit");
   // gROOT->ProcessLine(".x RooCBCrujffPdf.cxx+");
   //gROOT->ProcessLine(".x RooCBCBPdf.cxx+");
   cout << "mass = " << mass << endl; 
+  cout << "coupling = " << coupling << endl; 
   // runfits(mass, true);
-  runfits(mass, true, width, model);
+  runfits(mass, coupling, doBand, width);
 }
