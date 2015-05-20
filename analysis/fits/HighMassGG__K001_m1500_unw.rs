8bb3cf3e82484d99f158622d7e9bc2914c214aac
crovelli <crovelli@gmail.com>
2015-05-20 14:39:16 +0200

    moving to doubleCB fits

--- analysis/fits/HighMassGG__K001_m1500_unw.rs
+++ analysis/fits/HighMassGG__K001_m1500_unw.rs
@@ -1,37 +1,35 @@
 mgg[500,6000];
 mggGen[500,6000];
-nvtx[0,50];
 weight[0, 1000];
 eventClass[-10,10];
 
-ReducedMass_sig_mean_cat0[-0.005,-0.01,0.001];
-ReducedMass_sig_sigma_cat0[0.005, 0.001, 0.01];
-ReducedMass_sig_alphaCBpos_cat0[-2.5, -3., -0.1];
-ReducedMass_sig_alphaCBneg_cat0[0.5, 0., 3.]; 
-ReducedMass_sig_nCBpos_cat0[5., 5., 5.];    
-ReducedMass_sig_nCBneg_cat0[5., 5., 5.];     
-ReducedMass_sig_frac_cat0[0.5, 0., 1.];
+ReducedMass_sig_mean_cat0[1.0,0.5,1.5];
+ReducedMass_sig_sigma_cat0[0.01, 0.001, 0.2];
+ReducedMass_sig_alpha1_cat0[0.5, 0.1, 1.];
+ReducedMass_sig_alpha2_cat0[0.5, 0.1, 5.]; 
+ReducedMass_sig_n1_cat0[5., 1., 15.];    
+ReducedMass_sig_n2_cat0[5., 1., 15.];     
 
-ReducedMass_sig_mean_cat1[-0.003,-0.01,0.];
-ReducedMass_sig_sigma_cat1[0.006, 0.001, 0.01];
-ReducedMass_sig_alphaCBpos_cat1[-1., -3., -0.1];
-ReducedMass_sig_alphaCBneg_cat1[0.5, 0., 3.]; 
-ReducedMass_sig_nCBpos_cat1[5., 5., 5.];    
-ReducedMass_sig_nCBneg_cat1[5., 5., 5.];     
-ReducedMass_sig_frac_cat1[0.7, 0., 1.];
+ReducedMass_sig_mean_cat1[1.0,0.5,1.5];
+ReducedMass_sig_sigma_cat1[0.01, 0.001, 0.2];
+ReducedMass_sig_alpha1_cat1[0.5, 0.1, 1.];
+ReducedMass_sig_alpha2_cat1[0.5, 0.1, 5.]; 
+ReducedMass_sig_n1_cat1[5., 1., 15.];    
+ReducedMass_sig_n2_cat1[5., 1., 15.];     
 
-ReducedMass_sig_mean_cat2[-0.004.,-0.01, 0.];
-ReducedMass_sig_sigma_cat2[0.01, 0.001, 0.1];
-ReducedMass_sig_alphaCBpos_cat2[-1., -3., -0.1];
-ReducedMass_sig_alphaCBneg_cat2[0.5, 0., 1.5]; 
-ReducedMass_sig_nCBpos_cat2[5., 5., 5.];    
-ReducedMass_sig_nCBneg_cat2[5., 5., 5.];     
-ReducedMass_sig_frac_cat2[0.5, 0., 1.];
+ReducedMass_sig_mean_cat2[1.0,0.5,1.5];
+ReducedMass_sig_sigma_cat2[0.01, 0.001, 0.2];
+ReducedMass_sig_alpha1_cat2[0.5, 0.1, 1.];
+ReducedMass_sig_alpha2_cat2[0.5, 0.1, 5.]; 
+ReducedMass_sig_n1_cat2[5., 1., 15.];    
+ReducedMass_sig_n2_cat2[5., 1., 15.];     
 
-ReducedMass_sig_mean_cat3[-0.003,-0.01, 0.];
-ReducedMass_sig_sigma_cat3[0.01, 0.001, 0.1];
-ReducedMass_sig_alphaCBpos_cat3[-5., -6., -1.];
-ReducedMass_sig_alphaCBneg_cat3[2.6, 0., 4.];
-ReducedMass_sig_nCBpos_cat3[5., 5., 5.];    
-ReducedMass_sig_nCBneg_cat3[5., 5., 5.];     
-ReducedMass_sig_frac_cat3[0.5, 0., 1.];
+ReducedMass_sig_mean_cat3[1.0,0.5,1.5];
+ReducedMass_sig_sigma_cat3[0.01, 0.001, 0.2];
+ReducedMass_sig_alpha1_cat3[0.5, 0.1, 1.5];
+ReducedMass_sig_alpha2_cat3[0.5, 0.1, 5.]; 
+ReducedMass_sig_n1_cat3[5., 1., 15.];    
+ReducedMass_sig_n2_cat3[5., 1., 15.];     
+
+
+
