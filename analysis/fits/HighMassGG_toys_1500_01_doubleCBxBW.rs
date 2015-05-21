7eeb58a33b94fdc3e6e71109267447c2c9ab6f2d
crovelli <crovelli@gmail.com>
2015-05-21 11:12:59 +0200

    first commit

---
+++ analysis/fits/HighMassGG_toys_1500_01_doubleCBxBW.rs
@@ -0,0 +1,74 @@
+mgg[500,6000];
+mggGen[500,6000];
+weight[0, 1000];
+eventClass[-10,10];
+
+Sig_sigma_cat0[5.9816e-03];
+Sig_alpha1_cat0[8.3161e-01];
+Sig_alpha2_cat0[1.9525e+00];
+Sig_n1_cat0[4.2851e+00];
+Sig_n2_cat0[1.6494e+00];
+Sig_mean_cat0[9.9644e-01];
+meanBW_cat0[1.49997e+03];
+sigmaBW_cat0[2.15672e+01];
+
+Sig_sigma_cat1[6.0556e-03];
+Sig_alpha1_cat1[9.6761e-01];
+Sig_alpha2_cat1[1.8421e+00];
+Sig_n1_cat1[2.2568e+00];
+Sig_n2_cat1[1.7089e+00];
+Sig_mean_cat1[9.9681e-01];
+meanBW_cat1[1.49997e+03];
+sigmaBW_cat1[2.15672e+01];
+
+Sig_sigma_cat2[1.1985e-02];
+Sig_alpha1_cat2[9.4004e-01];
+Sig_alpha2_cat2[1.3737e+00];
+Sig_n1_cat2[7.8358e+00];
+Sig_n2_cat2[9.5203e+00];
+Sig_mean_cat2[9.9667e-01];
+meanBW_cat2[1.49997e+03];
+sigmaBW_cat2[2.15672e+01];
+
+Sig_sigma_cat3[1.1939e-02];
+Sig_alpha1_cat3[1.1424e+00];
+Sig_alpha2_cat3[1.7139e+00];
+Sig_n1_cat3[2.8587e+00];
+Sig_n2_cat3[3.2719e+00];
+Sig_mean_cat3[9.9623e-01];
+meanBW_cat3[1.49997e+03];
+sigmaBW_cat3[2.15672e+01];
+
+#SigF_mean_cat0[0.,-10.,1.];
+#SigF_sigma_cat0[10, 5., 15.];
+#SigF_alpha1_cat0[1., 0.7, 1.2];
+#SigF_alpha2_cat0[1.7, 1.5, 2.];
+#SigF_n1_cat0[3., 2.5, 5.];
+#SigF_n2_cat0[2.5, 1., 5.];
+SigF_mean_cat0[-5.,-10.,1.];
+SigF_sigma_cat0[10, 5., 15.];
+SigF_alpha1_cat0[1., 0.1, 3.];
+SigF_alpha2_cat0[2., 0.1, 3.];
+SigF_n1_cat0[3., 1., 5.];
+SigF_n2_cat0[3., 1., 5.];
+
+SigF_mean_cat1[-5.,-10.,1.];
+SigF_sigma_cat1[10, 5., 15.];
+SigF_alpha1_cat1[1., 0.1, 3.];
+SigF_alpha2_cat1[2., 0.1, 3.];
+SigF_n1_cat1[3., 1., 5.];
+SigF_n2_cat1[3., 1., 5.];
+
+SigF_mean_cat2[-5.,-10.,1.];
+SigF_sigma_cat2[10, 0., 30.];
+SigF_alpha1_cat2[1., 0.1, 3.];
+SigF_alpha2_cat2[2., 0.1, 3.];
+SigF_n1_cat2[3., 1., 10.];
+SigF_n2_cat2[3., 1., 10.];
+
+SigF_mean_cat3[-5.,-10.,1.];
+SigF_sigma_cat3[10, 0., 30.];
+SigF_alpha1_cat3[1., 0.1, 3.];
+SigF_alpha2_cat3[2., 0.1, 3.];
+SigF_n1_cat3[3., 1., 10.];
+SigF_n2_cat3[3., 1., 10.];
