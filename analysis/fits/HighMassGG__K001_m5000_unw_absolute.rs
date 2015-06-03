ffad6a951f42a8056c03dd694ed701f912e3e14a
crovelli <crovelli@gmail.com>
2015-06-03 14:33:23 +0200

    new category names

--- analysis/fits/HighMassGG__K001_m5000_unw_absolute.rs
+++ analysis/fits/HighMassGG__K001_m5000_unw_absolute.rs
@@ -3,33 +3,33 @@ mggGen[500,6000];
 weight[0, 1000];
 eventClass[-10,10];
 
-Sig_mean_cat0[4950.,4940.,5000.];
-Sig_sigma_cat0[15, 5., 50.];
-Sig_alpha1_cat0[0.1, 0., 2.];
-Sig_alpha2_cat0[0.1, 0., 2.]; 
-Sig_n1_cat0[6., 5., 20.];    
-Sig_n2_cat0[6., 5., 20.];     
+Sig_mean_catEBHighR9[4950.,4940.,5000.];
+Sig_sigma_catEBHighR9[15, 5., 50.];
+Sig_alpha1_catEBHighR9[0.1, 0., 2.];
+Sig_alpha2_catEBHighR9[0.1, 0., 2.]; 
+Sig_n1_catEBHighR9[6., 5., 20.];    
+Sig_n2_catEBHighR9[6., 5., 20.];     
 
-Sig_mean_cat1[4950.,4940.,5000.];
-Sig_sigma_cat1[15, 5., 50.];
-Sig_alpha1_cat1[0.1, 0., 2.];
-Sig_alpha2_cat1[0.1, 0., 2.]; 
-Sig_n1_cat1[6., 1., 20.];    
-Sig_n2_cat1[6., 3., 20.];     
+Sig_mean_catEBLowR9[4950.,4940.,5000.];
+Sig_sigma_catEBLowR9[15, 5., 50.];
+Sig_alpha1_catEBLowR9[0.1, 0., 2.];
+Sig_alpha2_catEBLowR9[0.1, 0., 2.]; 
+Sig_n1_catEBLowR9[6., 1., 20.];    
+Sig_n2_catEBLowR9[6., 3., 20.];     
 
-Sig_mean_cat2[4950.,4940.,4980.];
-Sig_sigma_cat2[30, 5., 80.];
-Sig_alpha1_cat2[0.1, 0., 2.];
-Sig_alpha2_cat2[0.1, 0., 2.]; 
-Sig_n1_cat2[6., 1., 20.];    
-Sig_n2_cat2[1., 1., 20.];     
+Sig_mean_catEEHighR9[4950.,4940.,4980.];
+Sig_sigma_catEEHighR9[30, 5., 80.];
+Sig_alpha1_catEEHighR9[0.1, 0., 2.];
+Sig_alpha2_catEEHighR9[0.1, 0., 2.]; 
+Sig_n1_catEEHighR9[6., 1., 20.];    
+Sig_n2_catEEHighR9[1., 1., 20.];     
 
-Sig_mean_cat3[4960.,4940.,4990.];
-Sig_sigma_cat3[60, 40., 80.];
-Sig_alpha1_cat3[0.1, 0., 2.];
-Sig_alpha2_cat3[0.1, 0., 5.]; 
-Sig_n1_cat3[6., 1., 20.];    
-Sig_n2_cat3[6., 1., 20.];     
+Sig_mean_catEELowR9[4960.,4940.,4990.];
+Sig_sigma_catEELowR9[60, 40., 80.];
+Sig_alpha1_catEELowR9[0.1, 0., 2.];
+Sig_alpha2_catEELowR9[0.1, 0., 5.]; 
+Sig_n1_catEELowR9[6., 1., 20.];    
+Sig_n2_catEELowR9[6., 1., 20.];     
 
 
 
