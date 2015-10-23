d7713ffdabfdb984ae7c41a5c402c87a0d90d431
Margaret Zientek <mez34@cornell.edu>
2015-10-23 14:50:43 +0200

    fixed issue with 1st bins in pureweighting

--- analysis/macro/main.cpp
+++ analysis/macro/main.cpp
@@ -43,10 +43,10 @@ int main(){
   bool sortMC = false;		// use if want to sort bkg smallest to biggest
   bool doBlind = true;		// use to blind the analysis for Data (don't use distributions for met>100 & 110<mgg<130)
   bool makePURWfiles = false;	// recompute PURW and make files
-  bool doReweightPU = false;	// use PURW from old files if !makePURWfiles
-  bool doPlots = false;		// make plots for each sample individually
-  bool doComb = false;		// make stack/overlay plots
-  bool doABCD = true;		// run ABCD method 
+  bool doReweightPU = true;	// use PURW from old files if !makePURWfiles
+  bool doPlots = true;		// make plots for each sample individually
+  bool doComb = true;		// make stack/overlay plots
+  bool doABCD = false;		// run ABCD method 
 
   Double_t lumi = 150.0; // in pb^-1 
   UInt_t nBins_vtx = 40; // number of bins for PURW 
@@ -73,7 +73,7 @@ int main(){
   DblVec 	puweights_MC;
 
   // no puweight for data 
-  for (UInt_t i=1; i<=nBins_vtx; i++){ puweights_Data.push_back(1.0); }
+  for (UInt_t i=0; i<=nBins_vtx; i++){ puweights_Data.push_back(1.0); }
 
   if (doReweightPU){
     if (makePURWfiles){ 
@@ -103,7 +103,8 @@ int main(){
     }// end if makePURWfiles
 
     else{ // load PURW from already made files
-      TString fBkgName = Form("%spurw/PURW_MC.root",outDir.Data());
+      //TString fBkgName = Form("%spurw/PURW_MC.root",outDir.Data());
+      TString fBkgName = Form("PURW_zmumu.root",outDir.Data());
       TFile *fBkg = TFile::Open(fBkgName.Data());
       CheckValidFile(fBkg,fBkgName);
       TH1D *fBkgRatio = (TH1D*)fBkg->Get("nvtx_dataOverMC");  
@@ -115,7 +116,7 @@ int main(){
       //CheckValidTH1D(fSigRatio,"nvtx_dataOverMC",fSigName);
        
       for (UInt_t i=0; i<=nBins_vtx; i++){
-        puweights_MC.push_back(fBkgRatio->GetBinContent(i));
+        puweights_MC.push_back(fBkgRatio->GetBinContent(i+1));
         //puweights_sig.push_back(fSigRatio->GetBinContent(i));
       }
     }
